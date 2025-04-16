d071470ae84f2e0e8a75a6b408c78a85676c7457
mdyeasinhn <mdyeasinhn@gmail.com>
2025-04-16 22:15:19 +0600

    not found page

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+    return (
+        <div className='text-4xl text-center'>
+            404 Page Not Found!!
+        </div>
+    );
+};
+
+export default NotFoundPage;
