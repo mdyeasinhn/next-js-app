c3b01e1cf57a28272f70dd38376412a88aad39b4
mdyeasinhn <mdyeasinhn@gmail.com>
2025-06-21 22:32:25 +0600

    Set-up Json Server for Next Level Data Fetching

--- src/app/(withCommonLayout)/page.js
+++ src/app/(withCommonLayout)/page.js
@@ -1,18 +1,28 @@
-"use client"
-import { useRouter } from 'next/navigation';
+import ProductCard from '../../components/Products/ProductCard.jsx'
+//import { useRouter } from 'next/navigation';
 import React from 'react';
 
-const HomePage = () => {
-  const router = useRouter();
-  const handleNavigation = () => {
-    router.push('/dashboard')
-  }
+const HomePage = async() => {
+//  const router = useRouter();
+  // const handleNavigation = () => {
+  //   router.push('/dashboard')
+  // }
 
+ const res = await fetch("http://localhost:5000/products");
+ const products = await res.json();
+ console.log(products)
   return (
     <div>
       <div>
-      <button onClick={handleNavigation} className='mt-5'>Dashboard</button>
+      {/* <button onClick={handleNavigation} className='mt-5'>Dashboard</button> */}
+      <h2 className='text-4xl text-center mt-2'>Data Fetching, Caching, and Revalidating</h2>
       </div>
+
+      <div className='grid grid-cols-4 gap-8 w-[90%] mx-auto mt-10'>
+        {products.map((product)=>
+        <ProductCard key={product} product={product}/>)}
+      </div>
+
     </div>
   );
 };
